fe47ae50c15d1d48967837a910ee8a9c14519f02
zhouyidhu <100521585+zhouyidhu@users.noreply.github.com>
2022-02-27 22:32:27 +0800

    Add files via upload

---
+++ MatplotlibWidget_2.py
@@ -0,0 +1,35 @@
+import sys
+import matplotlib
+matplotlib.use("Qt5Agg")
+from PyQt5.QtWidgets import QApplication, QVBoxLayout, QSizePolicy, QWidget
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import numpy as np
+class MyFigure(FigureCanvas):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        plt.rcParams['font.family'] = ['SimHei']
+        plt.rcParams['axes.unicode_minus'] = False
+        self.F = Figure(figsize=(width, height), dpi=100)
+        #self.F.subplots_adjust(top=1, bottom=0, left=0, right=1, hspace=0, wspace=0)
+        self.axes = self.F.add_subplot(111)
+        #self.axes1 = self.fig.add_subplot(2, 1, 2)
+        self.axes.hold(False)
+
+        FigureCanvas.__init__(self, self.F)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QSizePolicy.Expanding,
+                                   QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+    def plotcos(self):
+        t = np.arange(0.0, 3.0, 0.01)
+        s = np.sin(2 * np.pi * t)
+        self.F.axes.plot(t, s)
+        self.gridlayout = QGridLayout(self.widget_2)  # 继承容器widget
+        # 在布局中添加F这个实例
+        self.gridlayout.addWidget(self.F)
+
